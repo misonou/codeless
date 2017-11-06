9b3d6eeece992aafb2bf22a908ddadfcd317abf9
takerusilt <takeru.moe@gmail.com>
2017-11-06 17:58:25 +0800

    Fix using

--- src/Codeless.SharePoint/SharePoint/SPListItemAdapterBase.cs
+++ src/Codeless.SharePoint/SharePoint/SPListItemAdapterBase.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.Globalization;
 using System.Linq;
 using System.Threading;
