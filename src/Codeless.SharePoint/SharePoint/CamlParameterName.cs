1f98585b4ccb5618d621aab1543c5f3aad10bbe5
Takeru Lee <takeru.moe@gmail.com>
2017-11-17 15:26:39 +0800

    Split files

---
+++ src/Codeless.SharePoint/SharePoint/CamlParameterName.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codeless.SharePoint {
+  /// <summary>
+  /// Indicates the name of a parameter which its value can be binded after.
+  /// </summary>
+  public struct CamlParameterName {
+    internal static readonly CamlParameterName NoBinding = default(CamlParameterName);
+
+    internal readonly string Value;
+
+    /// <summary>
+    /// Creates an instance of the <see cref="CamlParameterName"/> class.
+    /// </summary>
+    /// <param name="value"></param>
+    public CamlParameterName(string value) {
+      CommonHelper.ConfirmNotNull(value, "value");
+      this.Value = value;
+    }
+
+    /// <summary>
+    /// Implicitly converts the name of a parameter specified by this instance to a string representation.
+    /// </summary>
+    /// <param name="p"></param>
+    /// <returns></returns>
+    public static implicit operator string(CamlParameterName p) {
+      return p.Value;
+    }
+  }
+}
---
+++ src/Codeless.SharePoint/SharePoint/ICamlParameterBinding.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codeless.SharePoint {
+  /// <summary>
+  /// Exposes properties and methods related to a parameter in a CAML expression.
+  /// </summary>
+  public interface ICamlParameterBinding {
+    /// <summary>
+    /// Gets a boolean value indicating whether this instance binds to any given arguments.
+    /// </summary>
+    bool IsParameter { get; }
+    /// <summary>
+    /// Gets the name of this parameter. <see cref="CamlParameterName.NoBinding"/> is returned if this instance does not bind to any given arguments.
+    /// </summary>
+    CamlParameterName ParameterName { get; }
+    /// <summary>
+    /// Gets the value type this parameter representing.
+    /// </summary>
+    CamlValueType ValueType { get; }
+    /// <summary>
+    /// Binds a single value and returns a string representation of the value from a collection of parameter values.
+    /// </summary>
+    /// <param name="bindings"></param>
+    /// <returns></returns>
+    string Bind(Hashtable bindings);
+    /// <summary>
+    /// Bings a list of values and returns a string representation of the values from a collection of parameter values.
+    /// </summary>
+    /// <param name="bindings"></param>
+    /// <returns></returns>
+    IEnumerable<string> BindCollection(Hashtable bindings);
+  }
+}
