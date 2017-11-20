1ae74d6644e6aae154562b59fbae35ce3f4ca63d
takerusilt <takeru.moe@gmail.com>
2017-11-20 17:19:24 +0800

    Rewrite CamlParameterBinding to support late value binders

---
+++ src/Codeless.SharePoint/SharePoint/ISPObjectContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.SharePoint;
+using Microsoft.SharePoint.Taxonomy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Codeless.SharePoint {
+  /// <summary>
+  /// Represents data source of SharePoint objects.
+  /// </summary>
+  public interface ISPObjectContext {
+    /// <summary>
+    /// Gets the site collection where persisted SharePoint objects to be retrieved from.
+    /// </summary>
+    SPSite Site { get; }
+    /// <summary>
+    /// Gets the term store where persisted objects of the Managed Metadata Service to be retrieved from.
+    /// </summary>
+    TermStore TermStore { get; }
+  }
+}
