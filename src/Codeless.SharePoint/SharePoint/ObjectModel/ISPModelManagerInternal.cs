39d245efdbc97e6d8e8178312f3cb7dfee98e776
Takeru Lee <takeru.moe@gmail.com>
2017-11-27 16:34:42 +0800

    Reduce load of model manager ctor + minor refactor

--- src/Codeless.SharePoint/SharePoint/ObjectModel/ISPModelManagerInternal.cs
+++ src/Codeless.SharePoint/SharePoint/ObjectModel/ISPModelManagerInternal.cs
@@ -4,6 +4,7 @@ namespace Codeless.SharePoint.ObjectModel {
   internal interface ISPModelManagerInternal : ISPModelManager {
     SPModelDescriptor Descriptor { get; }
     SPObjectCache ObjectCache { get; set; }
+    SPModelImplicitQueryMode ImplicitQueryMode { get; }
     IEnumerable<SPModelUsage> ContextLists { get; }
 
     SPModel TryCreateModel(ISPListItemAdapter item, bool readOnly);
